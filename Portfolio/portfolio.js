5eb5aa5b650579fb36b9f3d8287d2167c7882633
Bibas <251724816+itsmebibas@users.noreply.github.com>
2026-07-12 15:04:26 +0545

    Sidebar change ver to Horizonatal and comment out theme

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -1,13 +1,13 @@
-let btn = document.querySelector("#mode");
-let bd = document.body;
-if (localStorage.getItem("theme") == "dark") {
-  bd.classList.add("dark");
-}
-btn.addEventListener("click", () => {
-  bd.classList.toggle("dark");
-  if (bd.classList.contains("dark")) {
-    localStorage.setItem("theme", "dark");
-  } else {
-    localStorage.setItem("theme", "light");
-  }
-});
+// let btn = document.querySelector("#mode");
+// let bd = document.body;
+// if (localStorage.getItem("theme") == "dark") {
+//   bd.classList.add("dark");
+// }
+// btn.addEventListener("click", () => {
+//   bd.classList.toggle("dark");
+//   if (bd.classList.contains("dark")) {
+//     localStorage.setItem("theme", "dark");
+//   } else {
+//     localStorage.setItem("theme", "light");
+//   }
+// });
